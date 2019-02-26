236bf571ade87247b30ccef16fd8a45f768bb68e
hbsun2113 <sun710467272@outlook.com>
2019-02-26 09:56:50 +0800

    bk before push

---
+++ dgl/cmake-build-debug/CMakeFiles/dgl.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/v-haosun/PycharmProjects/myPytorch/dgl/src/c_api_common.cc" "C:/Users/v-haosun/PycharmProjects/myPytorch/dgl/cmake-build-debug/CMakeFiles/dgl.dir/src/c_api_common.cc.obj"
+  "C:/Users/v-haosun/PycharmProjects/myPytorch/dgl/src/graph/graph.cc" "C:/Users/v-haosun/PycharmProjects/myPytorch/dgl/cmake-build-debug/CMakeFiles/dgl.dir/src/graph/graph.cc.obj"
+  "C:/Users/v-haosun/PycharmProjects/myPytorch/dgl/src/graph/graph_apis.cc" "C:/Users/v-haosun/PycharmProjects/myPytorch/dgl/cmake-build-debug/CMakeFiles/dgl.dir/src/graph/graph_apis.cc.obj"
+  "C:/Users/v-haosun/PycharmProjects/myPytorch/dgl/src/graph/graph_op.cc" "C:/Users/v-haosun/PycharmProjects/myPytorch/dgl/cmake-build-debug/CMakeFiles/dgl.dir/src/graph/graph_op.cc.obj"
+  "C:/Users/v-haosun/PycharmProjects/myPytorch/dgl/src/graph/hbsunTest.cpp" "C:/Users/v-haosun/PycharmProjects/myPytorch/dgl/cmake-build-debug/CMakeFiles/dgl.dir/src/graph/hbsunTest.cpp.obj"
+  "C:/Users/v-haosun/PycharmProjects/myPytorch/dgl/src/graph/immutable_graph.cc" "C:/Users/v-haosun/PycharmProjects/myPytorch/dgl/cmake-build-debug/CMakeFiles/dgl.dir/src/graph/immutable_graph.cc.obj"
+  "C:/Users/v-haosun/PycharmProjects/myPytorch/dgl/src/graph/traversal.cc" "C:/Users/v-haosun/PycharmProjects/myPytorch/dgl/cmake-build-debug/CMakeFiles/dgl.dir/src/graph/traversal.cc.obj"
+  "C:/Users/v-haosun/PycharmProjects/myPytorch/dgl/src/runtime/c_runtime_api.cc" "C:/Users/v-haosun/PycharmProjects/myPytorch/dgl/cmake-build-debug/CMakeFiles/dgl.dir/src/runtime/c_runtime_api.cc.obj"
+  "C:/Users/v-haosun/PycharmProjects/myPytorch/dgl/src/runtime/cpu_device_api.cc" "C:/Users/v-haosun/PycharmProjects/myPytorch/dgl/cmake-build-debug/CMakeFiles/dgl.dir/src/runtime/cpu_device_api.cc.obj"
+  "C:/Users/v-haosun/PycharmProjects/myPytorch/dgl/src/runtime/dso_module.cc" "C:/Users/v-haosun/PycharmProjects/myPytorch/dgl/cmake-build-debug/CMakeFiles/dgl.dir/src/runtime/dso_module.cc.obj"
+  "C:/Users/v-haosun/PycharmProjects/myPytorch/dgl/src/runtime/file_util.cc" "C:/Users/v-haosun/PycharmProjects/myPytorch/dgl/cmake-build-debug/CMakeFiles/dgl.dir/src/runtime/file_util.cc.obj"
+  "C:/Users/v-haosun/PycharmProjects/myPytorch/dgl/src/runtime/module.cc" "C:/Users/v-haosun/PycharmProjects/myPytorch/dgl/cmake-build-debug/CMakeFiles/dgl.dir/src/runtime/module.cc.obj"
+  "C:/Users/v-haosun/PycharmProjects/myPytorch/dgl/src/runtime/module_util.cc" "C:/Users/v-haosun/PycharmProjects/myPytorch/dgl/cmake-build-debug/CMakeFiles/dgl.dir/src/runtime/module_util.cc.obj"
+  "C:/Users/v-haosun/PycharmProjects/myPytorch/dgl/src/runtime/ndarray.cc" "C:/Users/v-haosun/PycharmProjects/myPytorch/dgl/cmake-build-debug/CMakeFiles/dgl.dir/src/runtime/ndarray.cc.obj"
+  "C:/Users/v-haosun/PycharmProjects/myPytorch/dgl/src/runtime/registry.cc" "C:/Users/v-haosun/PycharmProjects/myPytorch/dgl/cmake-build-debug/CMakeFiles/dgl.dir/src/runtime/registry.cc.obj"
+  "C:/Users/v-haosun/PycharmProjects/myPytorch/dgl/src/runtime/system_lib_module.cc" "C:/Users/v-haosun/PycharmProjects/myPytorch/dgl/cmake-build-debug/CMakeFiles/dgl.dir/src/runtime/system_lib_module.cc.obj"
+  "C:/Users/v-haosun/PycharmProjects/myPytorch/dgl/src/runtime/thread_pool.cc" "C:/Users/v-haosun/PycharmProjects/myPytorch/dgl/cmake-build-debug/CMakeFiles/dgl.dir/src/runtime/thread_pool.cc.obj"
+  "C:/Users/v-haosun/PycharmProjects/myPytorch/dgl/src/runtime/threading_backend.cc" "C:/Users/v-haosun/PycharmProjects/myPytorch/dgl/cmake-build-debug/CMakeFiles/dgl.dir/src/runtime/threading_backend.cc.obj"
+  "C:/Users/v-haosun/PycharmProjects/myPytorch/dgl/src/runtime/workspace_pool.cc" "C:/Users/v-haosun/PycharmProjects/myPytorch/dgl/cmake-build-debug/CMakeFiles/dgl.dir/src/runtime/workspace_pool.cc.obj"
+  "C:/Users/v-haosun/PycharmProjects/myPytorch/dgl/src/scheduler/scheduler.cc" "C:/Users/v-haosun/PycharmProjects/myPytorch/dgl/cmake-build-debug/CMakeFiles/dgl.dir/src/scheduler/scheduler.cc.obj"
+  "C:/Users/v-haosun/PycharmProjects/myPytorch/dgl/src/scheduler/scheduler_apis.cc" "C:/Users/v-haosun/PycharmProjects/myPytorch/dgl/cmake-build-debug/CMakeFiles/dgl.dir/src/scheduler/scheduler_apis.cc.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../third_party/dlpack/include"
+  "../third_party/dmlc-core/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
